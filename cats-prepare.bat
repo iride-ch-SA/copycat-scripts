130141d94089cf87ee7acada1956fab241971848
Tiziano Besomi <tiziano.besomi@iridech.net>
2025-02-19 11:30:40 +0100

    Added template parameter for Acronis Snap Deploy Agent to cats-prepare

--- cats-prepare.bat
+++ cats-prepare.bat
@@ -1,3 +1,6 @@
+@echo off
+SET "PARAM=%~1"
+
 REM **** Update Scripts
 git --git-dir="C:\Admin\Scripts\.git" --work-tree="C:\Admin\Scripts" pull
 
@@ -16,3 +19,9 @@ powercfg /X monitor-timeout-ac 0
 powercfg /X standby-timeout-ac 0
 powercfg /X hibernate-timeout-ac 0
 powercfg /hibernate off
+
+IF /I "%PARAM%"=="template" (
+	REM **** Install Acronis Agent
+	powershell -command "(new-object System.Net.WebClient).DownloadFile('https://storage.googleapis.com/01931185-232c-77a5-8e67-8751490ebf3e/CopyCats/AcronisSnapDeployAgent64.msi','C:\Admin\Installers\AcronisSnapDeployAgent64.msi')"
+	C:\Admin\Installers\AcronisSnapDeployAgent64.msi /quiet
+)
