d65ee3d87351b1c983df85361c59c102811fc24f
Tiziano Besomi <tiziano.besomi@iridech.net>
2025-02-20 15:13:02 +0100

    Bug-fix tod deploy-userlogin

--- deploy-userlogin.bat
+++ deploy-userlogin.bat
@@ -14,6 +14,5 @@ REM Create task to run it at each user login
 set taskName="Run userlogin script"
 schtasks /create /tn %taskName% /tr %userlogin_file_path% /sc onlogon /f
 
-@echo on
 echo Rember to change to Group Users the scheduled task for userlogin.bat
 taskschd.msc
