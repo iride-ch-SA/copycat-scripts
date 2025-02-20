e9da5b03dcd372aaa3bf6d1f5f015894c5cd6f2d
Tiziano Besomi <tiziano.besomi@iridech.net>
2025-02-20 14:45:01 +0100

    set-background
    Modified for user-based, picture-based, default solid

--- set-background.bat
+++ set-background.bat
@@ -1,4 +1,12 @@
 @echo off
 IF EXIST "C:\Admin\Scripts\config\%username%.bgi" (
-	C:\Admin\Apps\BGInfo\Bginfo.exe C:\Admin\Scripts\config\%username%.bgi /timer:0 /silent
+	C:\Admin\Apps\Bginfo.exe C:\Admin\Scripts\config\%username%.bgi /timer:0 /silent
+) else (
+	IF EXIST "C:\Admin\Others\background.jpg" (
+		C:\Admin\Apps\Bginfo.exe C:\Admin\Scripts\config\background.bgi /timer:0 /silent
+	) else (
+		IF EXIST "C:\Admin\Scripts\config\solid-color.bgi" (
+			C:\Admin\Apps\Bginfo.exe C:\Admin\Scripts\config\solid-color.bgi /timer:0 /silent
+		)
+	)
 )
