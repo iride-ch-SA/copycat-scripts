c976e14a00813b7dc7b030e9fccfa20075f3bbd7
Tiziano Besomi <tiziano.besomi@iridech.net>
2025-02-20 16:11:23 +0100

    Added cats-clean

---
+++ cats-clean.bat
@@ -0,0 +1,40 @@
+@echo off
+setlocal enabledelayedexpansion
+set "do-restart=0"
+
+for %%a in (%*) do (
+	if /I "%%a"=="win-updates" (
+		REM **** Clean Windows updates
+		net stop wuauserv
+		net stop bits
+		del /f /s /q %windir%\SoftwareDistribution\*
+		net start wuauserv
+		net start bits
+		set "dorestart=1"
+	)
+	
+	if /I "%%a"=="sfc" (
+		sfc /scannow
+	)
+
+	if /I "%%a"=="dism-online" (
+		DISM /Online /Cleanup-Image /AnalyzeComponentStore
+		DISM /Online /Cleanup-Image /StartComponentCleanup /ResetBase
+		DISM /Online /Cleanup-Image /RestoreHealth
+	)
+	
+	if /I "%%a"=="network" (
+		ipconfig /release & ipconfig /renew
+		ipconfig /flushdns
+		powershell -noprofile -executionpolicy bypass -command "Get-NetConnectionProfile | Set-NetConnectionProfile -NetworkCategory Private"
+	)
+)
+
+if "!dorestart!"=="1" ( 
+	echo Restart is required by the script, presse enter to restart.
+	pause
+	shutdown /r /t 0 
+)
+
+
+
