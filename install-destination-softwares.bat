dc617bc1aa4e49ced568285390bbdce43c140a68
Tiziano Besomi <tiziano.besomi@iridech.net>
2025-02-24 20:23:34 +0100

    Bug fix

--- install-destination-softwares.bat
+++ install-destination-softwares.bat
@@ -2,7 +2,7 @@
 setlocal enabledelayedexpansion
 
 for %%a in (%*) do (
-	f /I "%%a"=="template" (
+	if /I "%%a"=="template" (
 		REM **** Install Acronis Agent
 		powershell -command "(new-object System.Net.WebClient).DownloadFile('https://storage.googleapis.com/01931185-232c-77a5-8e67-8751490ebf3e/CopyCats/AcronisSnapDeployAgent64.msi','C:\Admin\Installers\AcronisSnapDeployAgent64.msi')"
 		C:\Admin\Installers\AcronisSnapDeployAgent64.msi /quiet
