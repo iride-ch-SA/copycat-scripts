e888c549b1b6269215fc4b1bd7bf5ee5b06941fd
Tiziano Besomi <tiziano.besomi@iridech.net>
2025-02-28 11:36:43 +0100

    Added userlogin.bat search
    Search for local userlogins files to run on login

--- userlogin.bat
+++ userlogin.bat
@@ -1,2 +1,10 @@
 REM **** Set the background
 C:\Admin\Scripts\set-background.bat
+
+IF EXIST "C:\Admin\Others\%username%.bat" (
+	C:\Admin\Others\%username%.bat
+)
+
+IF EXIST "C:\Admin\Others\userlogin.bat" (
+	C:\Admin\Others\userlogin.bat
+)
