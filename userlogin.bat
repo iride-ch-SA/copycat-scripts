1ac715af00044f2c47ea8dff2fc84995c1dafd14
Tiziano Besomi <tiziano.besomi@iridech.net>
2025-02-28 12:38:50 +0100

    Refined userlogin script

--- userlogin.bat
+++ userlogin.bat
@@ -1,10 +1,13 @@
+@echo off
 REM **** Set the background
 call C:\Admin\Scripts\set-background.bat
 
 IF EXIST "C:\Admin\Others\%username%.bat" (
+	echo Calling local %username%.bat Script
 	call C:\Admin\Others\%username%.bat
 )
 
 IF EXIST "C:\Admin\Others\userlogin.bat" (
+	echo Calling local userlogin.bat Script
 	call C:\Admin\Others\userlogin.bat
 )
