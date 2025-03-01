c704809fd7817d51276c3c80c834be933d73bc2e
Tiziano Besomi <tiziano.besomi@iridech.net>
2025-03-01 16:01:11 +0100

    Added functionalities to set-registry
    Verify parameters
    aad-users show|hide
    local-user show|hide username

--- set-registry.bat
+++ set-registry.bat
@@ -1,5 +1,14 @@
 @echo off
+if "%~1"=="" (
+	echo ERROR: You must specify a scope as first parameter
+	exit /b 1
+)
 SET "PARAM1=%~1"
+
+if "%~2"=="" (
+	echo ERROR: You must specify a verb as second parameter
+	exit /b 1
+)
 SET "PARAM2=%~2"
 
 IF /I "%PARAM1%"=="news-and-interests" ( 
@@ -10,5 +19,33 @@ IF /I "%PARAM1%"=="news-and-interests" (
 	IF /I "%PARAM2%"=="enable" ( 
 		echo Enable AllowNewsAndInterests
 		reg delete "HKLM\SOFTWARE\Policies\Microsoft\Dsh" /v AllowNewsAndInterests /f 
+	)
+)
+
+IF /I "%PARAM1%"=="aad-users" ( 
+	IF /I "%PARAM2%"=="show" (
+		reg add "HKLM\Software\Policies\Microsoft\Windows\System" /t REG_DWORD /v EnumerateLocalUsers /d "1" /f
+		reg delete "HKLM\Software\Policies\Microsoft\Windows\System" /v DontEnumerateConnectedUsers /f
+		reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /t REG_DWORD /v dontdisplaylastusername /d "0" /f
+	) 
+	IF /I "%PARAM2%"=="hide" (
+		reg delete "HKLM\Software\Policies\Microsoft\Windows\System" /v EnumerateLocalUsers /f
+		reg add "HKLM\Software\Policies\Microsoft\Windows\System"  /t REG_DWORD /v DontEnumerateConnectedUsers /d "1" /f
+		reg delete "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v dontdisplaylastusername /f
+	) 
+)
+
+IF /I "%PARAM1%"=="local-user" ( 
+	if "%~3"=="" (
+		echo ERROR: You must specify a user name as third parameter
+		exit /b 1
+	)
+	SET "PARAM3=%~3"
+	
+	IF /I "%PARAM2%"=="show" ( 
+		reg delete "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon\SpecialAccounts\UserList" /f /v %PARAM3%
+	)
+	IF /I "%PARAM2%"=="hide" ( 
+		reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon\SpecialAccounts\UserList" /t REG_DWORD /f /d 0 /v %PARAM3%
 	)
 )
