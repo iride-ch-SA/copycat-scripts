042bd97b883bbb1785474f6e3c29b685a70b1947
Tiziano Besomi <tiziano.besomi@iridech.net>
2025-03-01 16:08:46 +0100

    Added forcing to local-user show

--- set-registry.bat
+++ set-registry.bat
@@ -43,7 +43,7 @@ IF /I "%PARAM1%"=="local-user" (
 	SET "PARAM3=%~3"
 	
 	IF /I "%PARAM2%"=="show" ( 
-		reg delete "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon\SpecialAccounts\UserList" /v "%PARAM3%"
+		reg delete "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon\SpecialAccounts\UserList" /v "%PARAM3%" /f
 	)
 	IF /I "%PARAM2%"=="hide" ( 
 		reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon\SpecialAccounts\UserList" /t REG_DWORD /f /d 0 /v "%PARAM3%"
