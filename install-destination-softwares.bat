ba1e870f6f36a557fca6a421110abf92b4fbadf2
Tiziano Besomi <tiziano.besomi@iridech.net>
2025-03-01 17:18:20 +0100

    Updated link for Acronis installer

--- install-destination-softwares.bat
+++ install-destination-softwares.bat
@@ -4,7 +4,7 @@ setlocal enabledelayedexpansion
 for %%a in (%*) do (
 	if /I "%%a"=="template" (
 		REM **** Install Acronis Agent
-		powershell -command "(new-object System.Net.WebClient).DownloadFile('https://storage.googleapis.com/01931185-232c-77a5-8e67-8751490ebf3e/CopyCats/AcronisSnapDeployAgent64.msi','C:\Admin\Installers\AcronisSnapDeployAgent64.msi')"
+		powershell -command "(new-object System.Net.WebClient).DownloadFile('https://storage.googleapis.com/01931185-232c-77a5-8e67-8751490ebf3e/CopyCats/Admin/Installers/AcronisSnapDeployAgent64.msi','C:\Admin\Installers\AcronisSnapDeployAgent64.msi')"
 		C:\Admin\Installers\AcronisSnapDeployAgent64.msi /quiet
 	)
 	
