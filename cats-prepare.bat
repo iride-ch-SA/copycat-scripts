8491f415d943426fd4b9841395522f0c192dd9e1
Tiziano Besomi <tiziano.besomi@iridech.net>
2025-03-01 16:14:02 +0100

    cats-prepare fixed and cured

--- cats-prepare.bat
+++ cats-prepare.bat
@@ -1,58 +1,67 @@
 @echo off
 setlocal enabledelayedexpansion
 
-REM **** Consent Powershell Scripts
-powershell -Command "Set-ExecutionPolicy RemoteSigned -Scope CurrentUser -Force
+echo [96m----- Cats Prepare Script[0m [92mSTARTED[0m [96m-----[0m 
 
 for %%a in (%*) do (
 	if /I "%%a"=="scripts" (
-		REM **** Updates Scripts
-		git --git-dir="C:\Admin\Scripts\.git" --work-tree="C:\Admin\Scripts" pull
-		setx PATH "%PATH%;C:\Admin\Scripts" /M
+		echo [36mUpdate Scripts Folder[0m 
+		git --git-dir="C:\Admin\Scripts\.git" --work-tree="C:\Admin\Scripts" pull 
+		call C:\Admin\Scripts\set-path.bat
 	)
-	
+
 	if /I "%%a"=="admin-folders" (
-		REM **** Create Standard IT Admin folders in C:
-		mkdir C:\Admin
-		mkdir C:\Admin\Apps
-		mkdir C:\Admin\Drivers
-		mkdir C:\Admin\Installers
-		mkdir C:\Admin\Others
+		echo [36mPreparing/Upgrading C:\Admin folder structure[0m
+		if not exist "C:\Admin" ( mkdir C:\Admin )
+		if exist "C:\Admin" ( echo [32mOK:[0m C:\Admin ) else ( echo [31mERROR: C:\Admin wasn't created [0m )
+		if not exist "C:\Admin\Apps" ( mkdir C:\Admin\Apps )
+		if exist "C:\Admin\Apps" ( echo [32mOK:[0m C:\Admin\Apps ) else ( echo [31mERROR: C:\Admin\Apps wasn't created [0m )
+		if not exist "C:\Admin\Drivers" ( mkdir C:\Admin\Drivers )
+		if exist "C:\Admin\Drivers" ( echo [32mOK:[0m C:\Admin\Drivers ) else ( echo [31mERROR: C:\Admin\Drivers wasn't created [0m )
+		if not exist "C:\Admin\Installers" ( mkdir C:\Admin\Installers )
+		if exist "C:\Admin\Installers" ( echo [32mOK:[0m C:\Admin\Installers ) else ( echo [31mERROR: C:\Admin\Installers wasn't created [0m )
+		if not exist "C:\Admin\Others" ( mkdir C:\Admin\Others )
+		if exist "C:\Admin\Others" ( echo [32mOK:[0m C:\Admin\Others ) else ( echo [31mERROR: C:\Admin\Others wasn't created [0m )
 	)
-	
+
 	if /I "%%a"=="utils" (
-		REM **** Add Apps
-		winget install Microsoft.Sysinternals.BGInfo --location C:\Admin\Apps\
-		icacls "C:\Admin\Apps\bginfo.exe" /grant Users:(RX)
-		
-		REM **** Install Destination Softwares
-		C:\Admin\Scripts\install-destination-softwares.bat template base support
-		
-		REM **** Set a sagerun:1 (TODO: create REGISTRY ADD to do unattended)
+		echo [36mInstall Utilities[0m 
+
+		echo [36m Install BGInfo from install-destination-softwares[0m 
+		call C:\Admin\Scripts\install-destination-softwares.bat bginfo
+		echo [36m Install Template from install-destination-softwares[0m
+		call C:\Admin\Scripts\install-destination-softwares.bat template
+		echo [36m Install Base from install-destination-softwares[0m
+		call C:\Admin\Scripts\install-destination-softwares.bat base
+		echo [36m Install Support from install-destination-softwares[0m
+		call C:\Admin\Scripts\install-destination-softwares.bat support 
+
+		echo [36m Create a cleanmgr preset :1[0m
 		cleanmgr /sageset:1
 	)
-	
+
 	if /I "%%a"=="systailoring" (
-		REM **** Disable Widgets in menu bar (for all users)
-		reg add "HKLM\SOFTWARE\Policies\Microsoft\Dsh" /t REG_DWORD /v AllowNewsAndInterests /d "0" /f
-		
-		C:\Admin\Scripts\reset-power-settings.bat
+		echo [36mSet specific system parameters[0m 
+		echo [36mDisable Widgets in menu bar for all users[0m 
+		call C:\Admin\Scripts\set-registry.bat news-and-interests disable
+		echo [36mReset Power Settings to an always-on status[0m 
+		call C:\Admin\Scripts\reset-power-settings.bat
 	)
-	
+
 	if /I "%%a"=="win-updates" (
-		REM **** Install Powershell Windows Updates tool
+		echo [36mInstall Power Shell Windows Updates Tools[0m 
 		powershell -command "Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force"
 		powershell -command "Install-Module PSWindowsUpdate -Force"
 		powershell -command "Set-PSRepository -Name 'PSGallery' -InstallationPolicy Trusted"
 	)
-	
+
 	if /I "%%a"=="deploy-azure" (
-		REM **** Show AAD Users on Login Screen
-		reg add "HKLM\Software\Policies\Microsoft\Windows\System" /t REG_DWORD /v EnumerateLocalUsers /d "1" /f
-		reg delete "HKLM\Software\Policies\Microsoft\Windows\System" /v DontEnumerateConnectedUsers /f
-		reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /t REG_DWORD /v dontdisplaylastusername /d "0" /f
-		
-		REM **** Hide itadmin from Login Screen
-		reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon\SpecialAccounts\UserList" /t REG_DWORD /f /d 0 /v itadmin
+		echo [36mConfigure System for Azure joining[0m 
+		echo [36m Show AAD Users on Login Screen[0m
+		call C:\Admin\Scripts\set-registry.bat aad-users show
+		echo [36m Hide itadmin from Login Screen[0m
+		call C:\Admin\Scripts\set-registry.bat local-user hide itadmin
 	)
 )
+
+echo [96m----- Cats Prepare Script[0m [95mENDED[0m [96m-----[0m 
