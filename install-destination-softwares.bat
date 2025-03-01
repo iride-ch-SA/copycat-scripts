a8864168eb8d4f71f6c23b7d572e09722f1bbd45
Tiziano Besomi <tiziano.besomi@iridech.net>
2025-03-01 11:52:58 +0100

    Fix to install support
    Check if desktop symlink exist

--- install-destination-softwares.bat
+++ install-destination-softwares.bat
@@ -33,7 +33,9 @@ for %%a in (%*) do (
 		powershell -command "(new-object System.Net.WebClient).DownloadFile('https://storage.googleapis.com/01931185-232c-77a5-8e67-8751490ebf3e/SupportoIT/supportoit.exe','C:\Admin\Apps\supportoit.exe')"
 		
 		REM *** Create Shortcut for all Users
-		Powershell -command "New-Item -ItemType SymbolicLink -Path 'C:\Users\Public\Desktop' -Name 'Supporto IT' -Value 'C:\Admin\Apps\supportoit.exe'"
+		if not exist "C:\Users\Public\Desktop\Supporto IT" (
+			Powershell -command "New-Item -ItemType SymbolicLink -Path 'C:\Users\Public\Desktop' -Name 'Supporto IT' -Value 'C:\Admin\Apps\supportoit.exe'"
+		)
 	)
 	
 	if /I "%%a"=="intelnuc" (
