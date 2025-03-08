3de67f653f5e7b4d0d848f2906b6e465cb6c2cfa
Tiziano Besomi <tiziano.besomi@iridech.net>
2025-03-08 21:27:11 +0100

    Bettering cats script

--- cats-install.bat
+++ cats-install.bat
@@ -1,18 +1,44 @@
 @echo off
 setlocal enabledelayedexpansion
 
-echo [96m----- Cats Install Script[0m [92mSTARTED[0m [96m-----[0m 
+echo [95mSTARTING [96m : CopyCat Install[0m
 
 for %%a in (%*) do (
 	if /I "%%a"=="Adobe.Acrobat.Reader" (
 		if "%PROCESSOR_ARCHITECTURE%" == "AMD64" (
-			echo [36mInstalling Adobe Acrobat Reader 64bit using winget[0m
-			winget install Adobe.Acrobat.Reader.64-bit --accept-package-agreements --accept-source-agreements
+			winget list --id Adobe.Acrobat.Reader.64-bit | findstr /I "Adobe.Acrobat.Reader.64-bit" > nul
+			if %errorlevel% neq 0 (
+				echo [36mWINGET    : Installing Adobe Acrobat Reader 64bit [0m
+				winget install --id Adobe.Acrobat.Reader.64-bit --accept-package-agreements --accept-source-agreements
+			) else (
+				echo [36mWINGET    : Upgrading Adobe Acrobat Reader 64bit [0m
+				winget upgrade --id Adobe.Acrobat.Reader.64-bit
+			)
 		) else (
-			echo [36mInstalling Adobe Acrobat Reader 32bit using winget[0m
-			winget install Adobe.Acrobat.Reader.32-bit --accept-package-agreements --accept-source-agreements
+			winget list --id Adobe.Acrobat.Reader.32-bit | findstr /I "Adobe.Acrobat.Reader.32-bit" > nul
+			if %errorlevel% neq 0 (
+				echo [36mWINGET    : Installing Adobe Acrobat Reader 32bit [0m
+				winget install --id Adobe.Acrobat.Reader.32-bit --accept-package-agreements --accept-source-agreements
+			) else (
+				echo [36mWINGET    : Upgrading Adobe Acrobat Reader 64bit [0m
+				winget upgrade --id Adobe.Acrobat.Reader.32-bit
+			)
+		)
+	) else (
+		echo [33mWARNING   : Package %%a not found as copycat recipe, trying to pass directly to winget [0m
+		winget search %%a | findstr /I "%%a" > nul
+		if !errorlevel! neq 0 (
+			echo [31mERROR     : Package %%a cannot be found in winget. [0m
+		) else (
+			winget list --id Adobe.Acrobat.Reader.64-bit | findstr /I "Adobe.Acrobat.Reader.64-bit" > nul
+			if !errorlevel! neq 0 (
+				echo [36mWINGET    : Installing %%a [0m
+				winget install %%a --accept-package-agreements --accept-source-agreements
+			) else (
+				echo [36mWINGET    : Upgrading %%a [0m
+				winget upgrade %%a
+			)
 		)
 	)
 )
-
-echo [96m----- Cats Install Script[0m [95mENDED[0m [96m-----[0m 
+echo [92mDONE     [96m : CopyCat Install[0m
--- cats.bat
+++ cats.bat
@@ -1,32 +1,30 @@
 @echo off
 
-echo [96m----- CopyCat Scripts[0m [92mEXECUTING[0m [96m-----[0m 
+echo [95mEXECUTING[96m : CopyCat Scripts[0m
 
 :: Executing cats verbs and parameters
 if "%~1"=="" (
-	echo [31mERROR: You must specify a verb as first parameter [0m
-	echo [94mUsage: cats {install|uninstall|update|prepare|clean|set} [parameters] [0m
-	exit /b 1
+	echo [31mERROR     : You must specify a verb as first parameter [0m
+	echo [94mUSAGE     : cats install,uninstall,update,prepare,clean,set parameters [0m
 ) else if "%~1"=="install" (
 	:: Check if enough parameters are passed
 	if "%~2"=="" (
-		echo [31mERROR: You must specify at least one parameter for verb [91minstall [0m
-		echo [94mUsage: cats install [package-name]* [0m
-		exit /b 1
+		echo [31mERROR     : You must specify at least one parameter for verb [91minstall [0m
+		echo [94mUSAGE     : cats install package-names [0m
 	) else (
-		shift
-		call C:\Admin\Scripts\cats-install.bat "%*"
+		echo [36mCALLING   : cats install %2 %3 %4 %5 %6 %7 %8 %9 [0m
+		call C:\Admin\Scripts\cats-install.bat %2 %3 %4 %5 %6 %7 %8 %9
 	)
 ) else if "%~1"=="uninstall" (
-	
+	echo [36mCALLING   : cats-uninstall %2 %3 %4 %5 %6 %7 %8 %9 [0m
 ) else if "%~1"=="update" (
-	
+	echo [36mCALLING   : cats-update %2 %3 %4 %5 %6 %7 %8 %9 [0m
 ) else if "%~1"=="prepare" (
-	
+	echo [36mCALLING   : cats-prepare %2 %3 %4 %5 %6 %7 %8 %9 [0m
 ) else if "%~1"=="clean" (
-	
+	echo [36mCALLING   : cats-clean %2 %3 %4 %5 %6 %7 %8 %9 [0m
 ) else if "%~1"=="set" (
-	
+	echo [36mCALLING   : cats-set %2 %3 %4 %5 %6 %7 %8 %9 [0m
 )
 
-echo [96m----- CopyCats Scripts[0m [95mDONE[0m [96m-----[0m 
+echo [92mDONE     [96m : CopyCat Scripts[0m
