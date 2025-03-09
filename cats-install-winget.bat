1a14c4c48125237425cea8bacd49db7c16ad51bf
Tiziano Besomi <tiziano.besomi@iridech.net>
2025-03-09 10:27:08 +0100

    Fix in reading cmd result for cat install

--- cats-install-winget.bat
+++ cats-install-winget.bat
@@ -1,17 +1,29 @@
 @echo off
-winget search %~1 | findstr /I "%~1" > nul
-if %errorlevel% neq 0 (
-	echo [31mERROR     : Package %~1 cannot be found in winget. [0m
-	exit /b 2
-) else (
-	winget list --id %~1 | findstr /I "%~1" > nul
-	if %errorlevel% neq 0 (
-		echo [36mWINGET    : Installing %~1 [0m
-		winget install %~1 %~2 %~3 --accept-package-agreements --accept-source-agreements
-		exit /b 0
+
+set _wsearch=winget search %~1
+for /f "tokens=* delims=(=" %%s in ('%_wsearch% ^|find /I /c " %~1 "') do (
+	if %%s EQU 1 (
+		set _wlist=winget list --id %~1
+		for /f "tokens=* delims=(=" %%f in ('%_wlist% ^|find /I /c " %~1 "') do (
+			if %%f EQU 0 (
+				echo [36mWINGET    : Installing %~1 [0m
+				winget install %~1 %~2 %~3 --accept-package-agreements --accept-source-agreements
+				exit /b 0
+			) else (
+				echo [36mWINGET    : Upgrading %~1 [0m
+				winget upgrade %~1
+				exit /b 1
+			)
+		)
 	) else (
-		echo [36mWINGET    : Upgrading %~1 [0m
-		winget upgrade %~1
-		exit /b 1
+		if %%s GTR 1 (
+			echo [31mERROR     : Winget has more than one package for %~1. [0m
+			exit /b 2
+		) else (
+			echo [31mERROR     : Package %~1 cannot be found in winget. [0m
+			exit /b 2
+		)
 	)
 )
+
+exit /b 3
