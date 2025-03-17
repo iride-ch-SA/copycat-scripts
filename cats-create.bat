0289c4cf161d72e4a9480af263e562c8ec94dfaa
Tiziano Besomi <tiziano.besomi@iridech.net>
2025-03-17 22:10:03 +0100

    Bug Fix

--- cats-create.bat
+++ cats-create.bat
@@ -7,7 +7,7 @@ for %%a in (%*) do (
 	if exist C:\Admin\Scripts\cats-recipes\%%a.bat ( 
 		call "C:\Admin\Scripts\cats-recipes\%%a.bat" create %2 %3 %4 %5 %6 %7 %8 %9
 	) else (
-		if exist C:\Admin\Scripts\cats-recipes\Cats.%%a.bat ((
+		if exist C:\Admin\Scripts\cats-recipes\Cats.%%a.bat (
 			call "C:\Admin\Scripts\cats-recipes\Cats.%%a.bat" create %2 %3 %4 %5 %6 %7 %8 %9
 		)
 	)
--- cats-install.bat
+++ cats-install.bat
@@ -8,7 +8,7 @@ for %%a in (%*) do (
 	if exist C:\Admin\Scripts\cats-recipes\%%a.bat ( 
 		call C:\Admin\Scripts\cats-recipes\%%a.bat install %2 %3 %4 %5 %6 %7 %8 %9
 	) else (
-		if exist C:\Admin\Scripts\cats-recipes\Cats.%%a.bat ((
+		if exist C:\Admin\Scripts\cats-recipes\Cats.%%a.bat (
 			call "C:\Admin\Scripts\cats-recipes\Cats.%%a.bat" install %2 %3 %4 %5 %6 %7 %8 %9
 		) else (
 			set isShortCut=0
