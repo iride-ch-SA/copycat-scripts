94e6bbae2402fc2ed6445b04bf6e5f357011e9f0
Tiziano Besomi <tiziano.besomi@iridech.net>
2025-07-22 14:46:03 +0200

    Bug Fix HID Generator

--- cats-recipes/HID.bat
+++ cats-recipes/HID.bat
@@ -2,12 +2,17 @@
 setlocal enabledelayedexpansion
 
 if "%~1"=="create" (
-	SET PARAM="%~2"
 	
 	echo [32mRECIPE    : Executing HID Generator [0m
-	powershell -noprofile -executionpolicy bypass -command C:\Admin\Scripts\ps\hid-generator.ps1 "!PARAM!"
+	if "%~2"=="" (
+		powershell -noprofile -executionpolicy bypass -command C:\Admin\Scripts\ps\hid-generator.ps1
+	) else (
+		SET PARAM="%~2"
+		powershell -noprofile -executionpolicy bypass -command C:\Admin\Scripts\ps\hid-generator.ps1 "!PARAM!"
+	)
 	
 	echo [32mRECIPE    : Writing HID to C:\Admin\Others\HID.txt [0m
+	for /f "delims=" %%i in ('powershell -noprofile -executionpolicy bypass -command "C:\Admin\Scripts\ps\hid-generator.ps1"') do set HID=%%i
 	
 	if not exist "C:\Admin\Others" (
 		mkdir "C:\Admin\Others"
